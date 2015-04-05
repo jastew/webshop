28bced9e04b5ec87f3e51b9f7302996bb80e01d5
jayoe <jayoe08@gmail.com>
2015-04-05 12:08:29 +0100

    minor style changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,12 +20,26 @@ module.exports = function(grunt) {
 					'build/index.html' : 'jade/**/*.jade'
 				}
 			}
+		},
+		watch: {
+			sass: {
+				files: 'sass/**/*.sass',
+				tasks: ['sass']
+			},
+			jade: {
+				files: 'jade/**/*.jade',
+				tasks: ['jade']
+			}
 		}
 	});
 	     
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	// Default task(s).
-	grunt.registerTask('default', ['sass', 'jade']);
+	grunt.registerTask('default', ['sass', 'jade', 'watch']);
+	grunt.event.on('watch', function(action, filepath, target) {
+		grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+	});
 };
